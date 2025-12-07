f5f04e05752c2d9dfda3a4bc112ee1dc3004d4ad
doorii2 <ynyoung227@gmail.com>
2025-12-07 23:28:49 +0900

    issue 상세조회 수정

--- backend/src/github/services/issueService.ts
+++ backend/src/github/services/issueService.ts
@@ -1,19 +1,16 @@
 import { createGitHubClient } from "../client/githubClient";
 
 export const issueService = {
-  async getIssues(owner: string, repo: string, token: string) {
+  async getIssues(owner: string, repo: string, token: string): Promise<any[]> {
     const octokit = createGitHubClient(token);
 
-    const res = await octokit.request(
-      "GET /repos/{owner}/{repo}/issues",
-      {
-        owner,
-        repo,
-        per_page: 100,
-        state: "all",
-      }
-    );
+    const res = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+      owner,
+      repo,
+      per_page: 100,
+      state: "all",
+    });
 
-    return res.data;
+    return res.data.filter((item: any) => !item.pull_request);//pr필터링: issue, pr 같이 조회되는 문제 해결
   },
 };
